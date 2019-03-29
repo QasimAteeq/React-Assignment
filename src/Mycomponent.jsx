3144d197b34bb8b5504a8ab9e7a3eb990debfe75
QasimAteeq <qasimateeq1@gmail.com>
2019-03-29 14:42:28 +0500

    practice

---
+++ src/Mycomponent.jsx
@@ -0,0 +1,13 @@
+import React , {Component} from 'react';
+
+
+class Mycomponent extends Component{
+    render(){
+        return (
+            <div>
+            <h1>Practice</h1>
+            </div>
+        )
+    }
+}
+export default Mycomponent;
--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import Home from './Home';
 import Services from './Services';
 import Contact from './Contact';
 import Footer from './Footer';
-
+import Mycomponent from './Mycomponent';
 
 import * as serviceWorker from './serviceWorker';
 
@@ -15,6 +15,7 @@ ReactDOM.render(<Home />, document.getElementById('home'));
 ReactDOM.render(<Services />, document.getElementById('services'));
 ReactDOM.render(<Contact />, document.getElementById('contact'));
 ReactDOM.render(<Footer />, document.getElementById('footer'));
+ReactDOM.render(<Mycomponent />, document.getElementById('iso'));
 
 
 // If you want your app to work offline and load faster, you can change
